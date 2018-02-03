ea352cc1898a90f7c71b05086ae56c039631c04a
tpilat <tomek.p93@gmail.com>
2018-02-03 22:26:54 +0100

    Add tests for helper functions

--- include/utils.h
+++ include/utils.h
@@ -7,7 +7,8 @@ namespace aes {
     void subword(word w);
     void rotword(word w);
     void word_xor(word dst, const_word src);
-    std::array <uint8_t, internal::WORD_SIZE> get_rcon(size_t i);
+    std::array <uint8_t, internal::WORD_SIZE> get_rcon(size_t i); // to remove
     uint8_t xtime(uint8_t x);
 }
+
 #endif //CRYPTOAES_UTILS_H
---
+++ test/utils_test.cpp
@@ -0,0 +1,68 @@
+#include <utils.h>
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <limits>
+#include <numeric>
+#include <random>
+
+
+using aes::internal::WORD_SIZE;
+using ByteType = aes::word::value_type;
+using word_array = std::array<ByteType, WORD_SIZE>;
+
+
+TEST(UtilsTestCase, SubwordTest) {
+    using aes::internal::SBOX;
+    constexpr size_t words_in_sbox = SBOX.size() / WORD_SIZE;
+
+    for (size_t i = 0; i < words_in_sbox; ++i) {
+        word_array input_word{};
+        std::iota(input_word.begin(), input_word.end(), i * WORD_SIZE);
+        aes::subword(input_word);
+
+        aes::const_word correct_word{SBOX.data() + i * WORD_SIZE, WORD_SIZE};
+        EXPECT_EQ(aes::const_word{input_word}, correct_word);
+    }
+}
+
+TEST(UtilsTestCase, RotwordTest) {
+    word_array input_word{};
+    std::iota(input_word.begin(), input_word.end(), 0);
+    aes::rotword(input_word);
+
+    word_array correct_word{};
+    std::iota(correct_word.begin(), correct_word.end() - 1, 1);
+    *(correct_word.end()) = 0;
+    EXPECT_EQ(input_word, correct_word);
+}
+
+TEST(UtilsTestCase, WordXorTest) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, std::numeric_limits<ByteType>::max());
+    auto byte_rand = [&dis, &gen]() { return dis(gen); };
+
+    word_array first{};
+    std::generate(first.begin(), first.end(), byte_rand);
+    word_array second{};
+    std::generate(second.begin(), second.end(), byte_rand);
+
+    word_array input_word{};
+    std::copy(input_word.begin(), input_word.end(), first.begin());
+    aes::word_xor(input_word, second);
+
+    word_array correct_word{};
+    for (size_t i = 0; i < correct_word.size(); ++i) {
+        correct_word[i] = first[i] ^ second[i];
+    }
+    EXPECT_EQ(input_word, correct_word);
+}
+
+TEST(UtilsTestCase, XtimeTest) {
+    EXPECT_EQ(aes::xtime(0x01), 0x02);
+    EXPECT_EQ(aes::xtime(0x08), 0x10);
+    EXPECT_EQ(aes::xtime(0x80), 0x1B);
+    EXPECT_EQ(aes::xtime(0x1B), 0x36);
+}
